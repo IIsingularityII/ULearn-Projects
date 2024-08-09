038af0e7e4e999255f933df0cb34bb97f329ec45
SfarxX <alupachiov@mail.ru>
2024-08-09 20:31:55 +0300

    Checking frequency by day number of birthdate -> completed

--- Histogramm/HistogramTask.cs
+++ Histogramm/HistogramTask.cs
@@ -4,9 +4,24 @@ internal static class HistogramTask
 {
     public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
+        string title = $"Рождаемость людей с именем '{name}'";
+        string[] labels = new string[31];
+        for (int i = 0; i < 31; i++)
+        {
+            labels[i] = (i + 1).ToString();
+        }
+        double[] values = new double[31];
+        for (int i = 0; i < 31; i++)
+        {
+            foreach (var person in names)
+            {
+                if (person.Name.Equals(name) && person.BirthDate.Day == (i + 1) && person.BirthDate.Day != 1)
+                    values[i] += 1;
+            }
+        }
         return new HistogramData(
-            $"Рождаемость людей с именем '{name}'", 
-            new [] {"1"}, 
-            new[] {0d});
+            title,
+            labels,
+            values);
     }
 }
