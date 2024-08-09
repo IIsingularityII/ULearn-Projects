e7ee644481b0d8241b2b5b04d39378f9763e2174
SfarxX <alupachiov@mail.ru>
2024-08-09 20:14:03 +0300

    If else statements -> completed

--- Pluralize/PluralizeTask.cs
+++ Pluralize/PluralizeTask.cs
@@ -4,7 +4,8 @@ public static class PluralizeTask
 {
 	public static string PluralizeRubles(int count)
 	{
-		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-		return "руб.";
-	}
+        if (count % 10 == 1 && count % 100 != 11) return "рубль";
+        else if ((count % 10 >= 2 && count % 10 <= 4) && !(count % 100 >= 12 && count % 100 <= 14)) return "рубля";
+        else return "рублей";
+    }
 }
