d96c3c73bd1edef827f92961afe38f5689c23343
SfarxX <alupachiov@mail.ru>
2024-08-09 20:32:59 +0300

    Completed heatmap task

--- Histogramm/HeatmapTask.cs
+++ Histogramm/HeatmapTask.cs
@@ -4,10 +4,33 @@ internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
+        string title = "Пример карты интенсивностей";
+        string[] XLabels = new string[30];
+        for (int i = 0; i < XLabels.Length; i++)
+        {
+            XLabels[i] = (i + 2).ToString();
+        }
+        string[] YLabels = new string[12];
+        for (int i = 0; i < YLabels.Length; i++)
+        {
+            YLabels[i] = (i + 1).ToString();
+        }
+        double[,] heat = new double[30, 12];
+        for (int i = 0; i < 30; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                foreach (var person in names)
+                {
+                    if (person.BirthDate.Day == (i + 2) && person.BirthDate.Month == (j + 1))
+                        heat[i, j] += 1;
+                }
+            }
+        }
         return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+            title,
+            heat,
+            XLabels,
+            YLabels);
     }
 }
