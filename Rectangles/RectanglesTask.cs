72994441a3a68fe6e935d1b7d9bea0149e04cab0
SfarxX <alupachiov@mail.ru>
2024-08-09 20:17:59 +0300

    Checked coordinates -> completed

--- Rectangles/RectanglesTask.cs
+++ Rectangles/RectanglesTask.cs
@@ -8,20 +8,40 @@ public static class RectanglesTask
     public static bool AreIntersected(Rectangle r1, Rectangle r2)
     {
         // так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-        return true;
+        return (r1.Bottom >= r2.Top && r1.Top <= r2.Bottom) && (r1.Right >= r2.Left && r1.Left <= r2.Right);
     }
 
     // Площадь пересечения прямоугольников
     public static int IntersectionSquare(Rectangle r1, Rectangle r2)
     {
-        return 0;
+        if (AreIntersected(r1, r2))
+        {
+            int width = Math.Min(r1.Right, r2.Right) - Math.Max(r1.Left, r2.Left);
+            int height = Math.Min(r1.Bottom, r2.Bottom) - Math.Max(r1.Top, r2.Top);
+            return width * height;
+        }
+        else return 0;
     }
-
     // Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
     // Иначе вернуть -1
     // Если прямоугольники совпадают, можно вернуть номер любого из них.
     public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
     {
+        if (AreIntersected(r1, r2))
+        {
+            if (IsFirstInSecond(r1, r2)) return 0;
+            else if (IsSecondInFirst(r1, r2)) return 1;
+        }
         return -1;
+    }
+
+    private static bool IsFirstInSecond(Rectangle r1, Rectangle r2)
+    {
+        return r1.Right <= r2.Right && r1.Left >= r2.Left && r1.Top >= r2.Top && r1.Bottom <= r2.Bottom;
+    }
+
+    private static bool IsSecondInFirst(Rectangle r1, Rectangle r2)
+    {
+        return r2.Right <= r1.Right && r2.Left >= r1.Left && r2.Top >= r1.Top && r2.Bottom <= r1.Bottom;
     }
 }
